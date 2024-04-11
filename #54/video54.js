356b10395f04b374f416d35906db9e7a0f8f32b1
harshithur <115203695+harshithur@users.noreply.github.com>
2024-04-11 13:31:02 +0530

    Add files via upload
    javascript

---
+++ #54/video54.js
@@ -0,0 +1,19 @@
+alert("These is the box to sumbmit")
+
+var a =prompt("Enter your Number ");
+
+console.log("You entered 1");
+console.log("You entered 2");
+console.log("You entered 3");
+console.log("You entered 4");
+
+var isTrue=confirm("Are you shore to blast your computer....")
+
+if(isTrue){
+    console.log("Your computer is blasting......")
+}else{
+    console.log("your computer is not blasting...")
+}
+document.title="hey i am good";
+
+document.body.style.backgroundColor='red';
